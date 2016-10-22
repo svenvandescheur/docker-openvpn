2585e68088f5be6203f70a648a52168ead908737
Sven van de Scheur <svenvandescheur@gmail.com>
2016-10-22 14:34:48 +0200

    Change entrypoint script

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -16,15 +16,22 @@ set -o pipefail
 
 
 # Set up correct variables
-DNS=`cat /etc/hosts | grep dnsmasq | awk '{print $1}'`
+DNS=`cat /etc/hosts | grep dnsmasq | awk '{print $1}'` || true
 if [[ ! $DNS ]]
 then
+    echo "Using default DNS"
     DNS=8.8.8.8
 fi
 
 
 # Set dns configuration
-sed -ri "s/push dhcp-option DNS.+/push dhcp-option DNS $DNS/g" /etc/openvpn/openvpn.conf
+if [[ -f /etc/openvpn/openvpn.conf ]]
+then
+    sed -ri "s/push dhcp-option DNS.+/push dhcp-option DNS $DNS/g" /etc/openvpn/openvpn.conf
+else
+    echo "No openvpn configuration found!"
+    exit 1
+fi
 
 
 # Execute command
